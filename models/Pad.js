22aa19611b88213d322260cc2f9a387b06f1f1d9
Mohith Reddy <mmohithreddy204@gmail.com>
2025-09-20 20:27:58 +0530

    Fix GET pads: auto-create pad, never 404

--- models/Pad.js
+++ models/Pad.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const PadSchema = new mongoose.Schema({
-  encrypted: { type: String, required: true },
+  _id: { type: String },               // Pad code as _id
+  encrypted: { type: String, default: "" },
   updatedAt: { type: Date, default: Date.now },
 });
 
