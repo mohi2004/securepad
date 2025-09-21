679d43fde1a960c75c02b85ec3ad99d8715b1ed9
Mohith Reddy <mmohithreddy204@gmail.com>
2025-09-21 19:39:33 +0530

    a

--- client/server/models/pad.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const PadSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  encrypted: { type: String, default: "" },
-  updatedAt: { type: Date, default: Date.now },
-});
-
-export default mongoose.models.Pad || mongoose.model("Pad", PadSchema);
